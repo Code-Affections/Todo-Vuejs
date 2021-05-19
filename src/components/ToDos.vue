e96cf9406359721839daf4c0b8328588fd5093de
vishnu <vishnu.kumarps@claysys.net>
2021-05-19 13:30:34 +0530

    Vuex

---
+++ src/components/ToDos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-body" v-for="todo in allTodos" :key="todo.id">
+          {{ todo.title }}
+          <br>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "ToDos",
+  methods: {
+    ...mapActions(["fetchTodos"]),
+  },
+  created() {
+    this.fetchTodos();
+  },
+  computed: mapGetters(["allTodos"]),
+};
+</script>
+<style>
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,27 @@
+import axios from "axios";
 import { createStore } from "vuex";
+//import axios from "axios";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    todos: []
+  },
+  mutations: {
+    setTodos: (state, todos) => (state.todos = todos),
+  },
+  actions: {
+
+    async fetchTodos({ commit }) {
+      const response= await axios.get('https://jsonplaceholder.typicode.com/todos');
+      console.log(response.data);
+      commit('setTodos', response.data);
+    }
+   
+  },
+  modules: {
+
+  },
+  getters: {
+    allTodos: state => state.todos
+  }
 });
